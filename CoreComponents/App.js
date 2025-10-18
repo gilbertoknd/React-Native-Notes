40de57ee7985eeb1b687e85088c4db2e71725b84
Gilbertoknd <gilbertoknd5@gmail.com>
2025-10-17 23:15:58 -0300

    Estudos 17-10-25

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,15 +1,24 @@
-import { View } from 'react-native';
+import { View, Text, Image, ImageBackground} from 'react-native';
+const logoImg = require("./assets/adaptive-icon.png")
 
 /* 
 Por padrão a view ocupa apenas o espaço dos seus filhos, se não tiver nenhum filho, não ocupará nenhum lugar na tela.
 Ao colocar o parâmetro flex: 1, ela ocupará o espaço disponível na tela
 */
 export default function App() {
-  return <View style = {{flex: 1, backgroundColor: "plum"}}>
-    <View style = {{height: 200, width: 200, backgroundColor: "lightblue"}}>
-    </View>
+  return (
+  <View style={{flex: 1, backgroundColor: "plum", padding: 60}}> 
+    
+    <Image source={logoImg} style={{width: 300, height: 300}} ></Image>
+    
+    <Image source={{uri:"https://picsum.photos/300"}} 
+    style={{width: 300, height: 300}} >
+    </Image>
+    
+    <ImageBackground source={logoImg} style={{flex:1}}>
+      <Text>IMAGE TEXT</Text>
+    </ImageBackground>
 
-    <View style = {{height: 300, width: 300, backgroundColor: "lightgreen"}}>
-    </View>
   </View>
+  );
 }
