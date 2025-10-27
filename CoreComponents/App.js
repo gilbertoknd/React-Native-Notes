c064ff4ea36026c1f7cf60dd2680d24277817c25
Gilbertoknd <gilbertoknd5@gmail.com>
2025-10-27 12:58:28 -0300

    Commit

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,17 +1,23 @@
-import { View, Button } from 'react-native';
+import { useState } from 'react';
+import { View, Button, Modal, Text } from 'react-native';
 
 
 export default function App() {
+  //{Constante para controlar a visibilidade do Modal}
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
   <View style={{flex: 1, backgroundColor: "plum", padding: 60}}> 
-    <Button 
-      title="Press" 
-      onPress={() => console.log("Apertou o botao")}
-      color="green"
-      disabled
-    > 
-      Submit 
-    </Button>
+    {/*Ao pressionar, seta a const para true, ativando o modal*/}
+    <Button title='Ativando o Modal' onPress={() => setIsModalVisible(true)} />
+    
+    <Modal visible={isModalVisible}>
+      <View style={{flex: 1, backgroundColor: "skyblue", padding: 60}}>
+        <Text>Conteudo do Modal</Text>
+        {/*Ao pressionar, seta a const para false, desativando o modal*/}
+        <Button title='Fechar Modal' onPress={() => setIsModalVisible(false)} />
+      </View>
+    </Modal>
+
   </View>
   );
 }
